993052be83c8647fc3dc5c5e0013687769f26a2f
marko <markoffka.kuznetsov@mail.ru>
2024-09-23 13:07:01 +0700

    + second_homework

---
+++ second_homework/main.java
@@ -0,0 +1,18 @@
+package second_homework;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner chisla = new Scanner(System.in);
+        System.out.print("Введите коэффициент a: ");
+        double a = chisla.nextDouble();
+        System.out.print("Введите коэффициент b: ");
+        double b = chisla.nextDouble();
+        System.out.print("Введите коэффициент c: ");
+        double c = chisla.nextDouble();
+        
+    public static double triangle(double a, double b, double c) {}
+
+    }
+}
---
+++ second_homework/mainTest.java
@@ -0,0 +1,5 @@
+package second_homework;
+
+public class mainTest {
+    
+}
